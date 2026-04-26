89eae89bf1f24b1dcd0b2caa50a6d6e37fc4e3f2
SpeedFox03 <jordix2003@gmail.com>
2026-04-26 14:01:29 +0200

    pages fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://SpeedFox03.github.io',
+  base: 'https://github.com/SpeedFox03/emberfox-site', // le nom exact de ton repo
+});
